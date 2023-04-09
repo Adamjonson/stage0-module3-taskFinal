ef1b08656a4f98213c53af80daacb0552523ea43
Hp <adkhamjon.abd@gmail.com>
2023-04-09 08:54:59 +0200

    Completes the whole task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int four = number / 1000;
+        int three = number / 100;
+        int two = number / 10;
+        int one = number - four * 1000 - three * 100 - two * 10;
+        int sum = four + three + two + one;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int hundreds = number / 100;
+        int tens = (number - hundreds * 100) / 10;
+        int digits = (number - hundreds * 100 - tens * 10);
+        System.out.println(hundreds + "" + tens + "" + digits);
     }
 }
