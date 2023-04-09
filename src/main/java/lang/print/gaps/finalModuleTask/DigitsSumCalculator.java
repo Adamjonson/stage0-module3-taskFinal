48b40d549356062ee068653d87b03360f413ec9e
Hp <adkhamjon.abd@gmail.com>
2023-04-09 12:39:04 +0200

    Corrects mistakes

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,10 +3,11 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int four = number / 1000;
-        int three = (number - four) / 100;
-        int two = (number - four - three) / 10;
-        int one = (number - four - three - two);
+        int three = (number - four * 1000) / 100;
+        int two = (number - four * 1000 - three * 100) / 10;
+        int one = (number - four * 1000 - three * 100 - two * 10);
         int sum = four + three + two + one;
+
         System.out.println(sum);
     }
 }
